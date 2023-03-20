7baef94b55dd6f1ab260635199dc1fcbd8247ee0
Emon <emon.mhk69@gmail.com>
2023-03-21 00:11:11 +0600

    dynamic content dynamic style done

--- reactjs-core-concepts/src/App.js
+++ reactjs-core-concepts/src/App.js
@@ -1,6 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 5555;
+const singer = {
+    name:"Emon",
+    age:21,
+    job:"Singer"
+}
+
+const singerStyle ={
+  color:"red",
+  backgroundColor:"white"
+}
+
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +22,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
+        <h2 className="header">
+          Hello!! i am Emon.
+        </h2>
+
+        <p>I love programming {2222+number} times</p>
+        <p style={singerStyle}> Name: {singer.name}, Age: {singer.age}, Job: {singer.job}</p>
+
+        <p style={ {color:"white",backgroundColor:"violet",padding:"1rem"} }>Hello newbies!!!</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
