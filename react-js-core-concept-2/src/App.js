02cc3628c12c123626d7e4fc9629e79c8e4ae0f7
Emon <emon.mhk69@gmail.com>
2023-03-21 13:11:43 +0600

    loading data from user and display them

--- react-js-core-concept-2/src/App.js
+++ react-js-core-concept-2/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 function App() {
   
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
 
-      <h1>we can add element like this but this is not dynamic</h1>
+      {/* <h1>we can add element like this but this is not dynamic</h1>
       <MySelf></MySelf>
 
       <h2>We can also add component like this but this is not fully dynamic</h2>
@@ -22,19 +22,55 @@ function App() {
       <h2>This is a dynamic way of adding component</h2>
       {
         myProducts.map(prod=> <Product name={prod.name} price={prod.price}></Product>)
-      }
+      } */}
 
-      <h2>Count project start</h2>
-      
+      {/* <h2>Count project start</h2>
+
       <Counter></Counter>
 
-      <h2>Counter done</h2>
+      <h2>Counter done</h2> */}
+
+      <ExternalUser></ExternalUser>
       
 
     </div>
   );
 }
 
+
+function DisplayUser(props){
+  return(
+    <div className="products-container">
+      <h2>Name: {props.name}</h2>
+      <p>Email: {props.mail}</p>
+    </div>
+  );
+}
+
+function ExternalUser(){
+  const [users, setUsers] = useState([]);
+
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res=> res.json())
+    .then(data=> setUsers(data))
+  },[])
+
+  return(
+    <div className="">
+      <h2>External Users</h2>
+      <p>Totall User: {users.length}</p>
+      {
+        users.map(user=> <DisplayUser name={user.name} mail={user.email}></DisplayUser>)
+      }
+    </div>
+  );
+
+}
+
+
+
+/*
 function Counter(){
   const [count, setCount] = useState(0);
   const increseCount=()=>setCount(count+1);
@@ -65,5 +101,7 @@ function MySelf(){
     </div>
   )
 }
+*/
+
 
 export default App;
