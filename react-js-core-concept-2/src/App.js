9e51352f35d2cff24cb215b63a865c6f60d98978
Emon <emon.mhk69@gmail.com>
2023-03-21 12:02:37 +0600

    recap done

--- react-js-core-concept-2/src/App.js
+++ react-js-core-concept-2/src/App.js
@@ -2,24 +2,47 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  const myProducts=[
+    {name:"Computer",price:76000},
+    {name:"Mobile",price: 25000},
+    {name:"watch",price: 25000},
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>we can add element like this but this is not dynamic</h1>
+      <MySelf></MySelf>
+
+      <h2>We can also add component like this but this is not fully dynamic</h2>
+      <Product name="phone" price="12300"></Product>
+
+      <h2>This is a dynamic way of adding component</h2>
+      {
+        myProducts.map(prod=> <Product name={prod.name} price={prod.price}></Product>)
+      }
+
     </div>
   );
+}
+
+
+function Product(props){
+  return(
+    <div className="products-container">
+      <h2>Name: {props.name}</h2>
+      <p>Price: {props.price}</p>
+    </div>
+  );
+}
+
+function MySelf(){
+  return(
+    <div className="">
+      <h2>Name: Md.Emon</h2>
+      <p>Age: 21</p>
+    </div>
+  )
 }
 
 export default App;
