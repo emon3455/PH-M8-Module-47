b656c15a44c4ac11d6b321ab52169c58ca7145e5
Emon <emon.mhk69@gmail.com>
2023-03-21 11:16:17 +0600

    dynamically component added from array and array like object

--- reactjs-core-concepts/src/App.js
+++ reactjs-core-concepts/src/App.js
@@ -9,30 +9,73 @@ const singer = {
 }
 
 const singerStyle ={
-  color:"red",
-  backgroundColor:"white"
+  color:"blue",
+  backgroundColor:"gray",
+  textAlign:"center",
+  padding:"0.5rem"
 }
 
+const center={
+  textAlign:"center"
+}
+
+const students=["Emon","Elina","Tuspa","Rajon","Aminul","Rafi","Mohiny"];
+const friends=[
+  {name:"Elina",age:22},
+  {name:"Mohiny",age:21},
+  {name:"Sajjad",age:23},
+];
 
 function App() {
+
   return (
     <div className="">
       <Person></Person>
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
-      <h2>We can also add normal elements</h2>
+      <h2 style={{textAlign:"center"}}>We can also add normal elements</h2>
       <Friend></Friend>
-      <Friend></Friend>
+      <h2 style={singerStyle}> Lets explore Dynamic data in component with props</h2>
+
+      <Subject name="Web Pprogramming" code="CSE301"></Subject>
+      <Subject name="Data Com" code="CSE303"></Subject>
+      <Subject name="IPD-1" code="CSE324"></Subject>
+      
+      <h2>Displaying All students</h2>
+
+      {
+        students.map(student=> <Person name={student}></Person> )
+      }
+
+      <h1>All of My Good Friends are</h1>
+      {
+        friends.map(friend=>{
+          return(
+            <div className="box">
+              <h2>Name: {friend.name}</h2>
+              <h2>Age: {friend.age}</h2>
+            </div>
+          );
+        })
+      }
+
     </div>
   );
 }
 
-function Person(){
+
+function Subject(props){
+  return(
+    <div className="container">
+      <h2>Subject: {props.name}</h2>
+      <p>Code: {props.code}</p>
+    </div>
+  );
+}
+
+
+function Person(props){
    return (
     <div className="header">
-      <h2>Md. Emon</h2>
-      <p>I am a student i love programming </p>
+      <h2>Name: {props.name}</h2>
     </div>
    )
 }
